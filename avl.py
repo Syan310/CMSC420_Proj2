78f64c3863592243d9b81c80e9c8bef03fd73326
Syan310 <105128575+Syan310@users.noreply.github.com>
2023-10-06 15:00:39 -0400

    Update avl.py

--- avl.py
+++ avl.py
@@ -1,17 +1,14 @@
 import json
-from typing import List
+from typing import List, Tuple
 
 # DO NOT MODIFY!
 class Node():
-    def  __init__(self,
-                  key       : int,
-                  word      : str,
-                  leftchild,
-                  rightchild):
-        self.key        = key
-        self.word      = word
-        self.leftchild  = leftchild
+    def __init__(self, key: int, word: str, leftchild=None, rightchild=None):
+        self.key = key
+        self.word = word
+        self.leftchild = leftchild
         self.rightchild = rightchild
+        self.height = 1  # added height property to Node
 
 # DO NOT MODIFY!
 def dump(root: Node) -> str:
@@ -22,17 +19,22 @@ def dump(root: Node) -> str:
             "l": (_to_dict(node.leftchild) if node.leftchild is not None else None),
             "r": (_to_dict(node.rightchild) if node.rightchild is not None else None)
         }
-    if root == None:
+    if root is None:
         dict_repr = {}
     else:
         dict_repr = _to_dict(root)
-    return json.dumps(dict_repr,indent = 2)
+    return json.dumps(dict_repr, indent=2)
 
+# Helper Methods
 
 def height(node):
     if not node:
         return 0
-    return max(height(node.leftchild), height(node.rightchild)) + 1
+    return node.height
+
+def update_height(node):
+    if node:
+        node.height = 1 + max(height(node.leftchild), height(node.rightchild))
 
 def balance_factor(node):
     return height(node.leftchild) - height(node.rightchild)
@@ -41,12 +43,16 @@ def left_rotate(x):
     y = x.rightchild
     x.rightchild = y.leftchild
     y.leftchild = x
+    update_height(x)
+    update_height(y)
     return y
 
 def right_rotate(y):
     x = y.leftchild
     y.leftchild = x.rightchild
     x.rightchild = y
+    update_height(y)
+    update_height(x)
     return x
 
 def balance(node):
@@ -58,70 +64,27 @@ def balance(node):
         if balance_factor(node.rightchild) > 0:
             node.rightchild = right_rotate(node.rightchild)
         node = left_rotate(node)
+    else:
+        update_height(node)
     return node
+
+# Main Methods
 
 def insert(root, key, word):
     if not root:
-        return Node(key, word, None, None)
+        return Node(key, word)
     
-    # Standard BST insert
     if key < root.key:
         root.leftchild = insert(root.leftchild, key, word)
     else:
         root.rightchild = insert(root.rightchild, key, word)
     
-    # Balance the node again
     return balance(root)
 
-
-
-def insert_without_balance(root, key, word):
-    if not root:
-        return Node(key, word, None, None)
-    
-    if key < root.key:
-        root.leftchild = insert_without_balance(root.leftchild, key, word)
-    else:
-        root.rightchild = insert_without_balance(root.rightchild, key, word)
-    
+def bulkInsert(root, items: List[Tuple[int, str]]) -> Node:
+    for key, word in items:
+        root = insert(root, key, word)
     return root
-
-def inorder_traversal(root, nodes_list):
-    if not root:
-        return
-    inorder_traversal(root.leftchild, nodes_list)
-    nodes_list.append((root.key, root.word))
-    inorder_traversal(root.rightchild, nodes_list)
-
-def construct_avl_tree(nodes_list):
-    if not nodes_list:
-        return None
-    mid_idx = len(nodes_list) // 2
-    key, word = nodes_list[mid_idx]
-    root = Node(key, word, None, None)
-    root.leftchild = construct_avl_tree(nodes_list[:mid_idx])
-    root.rightchild = construct_avl_tree(nodes_list[mid_idx+1:])
-    return root
-
-# bulkInsert
-# The parameter items should be a list of pairs of the form [key,word] where key is an integer and word is a string.
-# For the tree rooted at root, first insert all of the [key,word] pairs as if the tree were a standard BST, with no balancing.
-# Then do a preorder traversal of the [key,word] pairs and use this traversal to build a new tree using AVL insertion.
-# Return the root
-def bulkInsert(root, items):
-    # Step 1: Standard BST insert without balancing
-    for item in items:
-        key, word = item
-        root = insert_without_balance(root, key, word)
-    
-    # Step 2: Extract all nodes in order
-    nodes_in_order = []
-    inorder_traversal(root, nodes_in_order)
-    
-    # Step 3: Construct a new balanced AVL tree
-    root = construct_avl_tree(nodes_in_order)
-    return root
-
 
 def inorder_traversal_filtered(root, nodes_list, exclude_keys):
     if not root:
@@ -130,46 +93,17 @@ def inorder_traversal_filtered(root, nodes_list, exclude_keys):
     if root.key not in exclude_keys:
         nodes_list.append((root.key, root.word))
     inorder_traversal_filtered(root.rightchild, nodes_list, exclude_keys)
-# bulkDelete
-# The parameter keys should be a list of keys.
-# For the tree rooted at root, first tag all the corresponding nodes (however you like),
-# Then do a preorder traversal of the [key,word] pairs, ignoring the tagged nodes,
-# and use this traversal to build a new tree using AVL insertion.
-# Return the root.
-def bulkDelete(root, keys):
-    keys_set = set(keys)  # for O(1) lookup
-    
-    # Step 1: Extract all nodes not in keys_set in order
+
+def bulkDelete(root, keys: List[int]) -> Node:
+    keys_set = set(keys)
     nodes_in_order = []
     inorder_traversal_filtered(root, nodes_in_order, keys_set)
-    
-    # Step 2: Construct a new balanced AVL tree
-    root = construct_avl_tree(nodes_in_order)
-    return root
+    new_root = None
+    for key, word in nodes_in_order:
+        new_root = insert(new_root, key, word)
+    return new_root
 
-
-
-
-def inorderTraversal(node, delete_keys):
-    """
-    Perform an inorder traversal of the tree rooted at node, 
-    returning a list of nodes (not in delete_keys) in sorted order.
-    """
-    result = []
-    if node:
-        result.extend(inorderTraversal(node.leftchild, delete_keys))
-        if node.key not in delete_keys:
-            result.append((node.key, node.word))
-        result.extend(inorderTraversal(node.rightchild, delete_keys))
-    return result
-
-
-# search
-# For the tree rooted at root, calculate the list of keys on the path from the root to the search_key,
-# including the search key, and the word associated with the search_key.
-# Return the json stringified list [key1,key2,...,keylast,word] with indent=2.
-# If the search_key is not in the tree return a word of None.
-def search(root, search_key):
+def search(root, search_key: int) -> str:
     path = []
     current = root
     while current:
@@ -182,13 +116,7 @@ def search(root, search_key):
             current = current.leftchild
     return json.dumps(None, indent=2)
 
-
-
-# replace
-# For the tree rooted at root, replace the word corresponding to the key search_key by replacement_word.
-# The search_key is guaranteed to be in the tree.
-# Return the root
-def replace(root, search_key, replacement_word):
+def replace(root, search_key: int, replacement_word: str) -> Node:
     if not root:
         return None
     if root.key == search_key:
@@ -198,5 +126,3 @@ def replace(root, search_key, replacement_word):
     else:
         root.leftchild = replace(root.leftchild, search_key, replacement_word)
     return root
-
-
